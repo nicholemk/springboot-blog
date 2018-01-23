8d9919f0eaa48d94f42c37870cd6938ffc151cc7
nicholekirlin <nichole.kirlin@gmail.com>
2018-01-23 16:21:58 -0600

    Add project meta files

---
+++ src/main/java/com/codeup/springbootblog/Controllers/SpringbootBlogApplication.java
@@ -0,0 +1,18 @@
+package com.codeup.springbootblog.Controllers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+@SpringBootApplication
+public class SpringbootBlogApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SpringbootBlogApplication.class, args);
+	}
+
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringbootBlogApplication.class);
+	}
+}
