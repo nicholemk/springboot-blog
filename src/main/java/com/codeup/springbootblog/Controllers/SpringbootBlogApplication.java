f564ca04552b065e5aa38378ef4bfb12d914d0bc
nicholekirlin <nichole.kirlin@gmail.com>
2018-01-24 08:41:36 -0600

    Add missing parent class

--- src/main/java/com/codeup/springbootblog/Controllers/SpringbootBlogApplication.java
+++ src/main/java/com/codeup/springbootblog/Controllers/SpringbootBlogApplication.java
@@ -3,9 +3,10 @@ package com.codeup.springbootblog.Controllers;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringbootBlogApplication {
+public class SpringbootBlogApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootBlogApplication.class, args);
